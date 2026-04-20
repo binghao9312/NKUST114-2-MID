115283994f1539d94de40a2a9413a8d330354569
unknown <binghao9312>
2026-04-20 11:22:27 +0800

    k

--- fix.py
+++
@@ -1,8 +0,0 @@
-
-with open('script.js', 'r', encoding='utf-8') as f:
-    d = f.read()
-d = d.replace(r'[\''', '[\'')
-d = d.replace(r'\']', '\']')
-with open('script.js', 'w', encoding='utf-8') as f:
-    f.write(d)
-
